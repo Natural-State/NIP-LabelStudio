cd11bba0995ac7c85dc1153a17fe1e0a9d2b1209
Ross Tyzack Pitman <rpitman@tyzacklabs.org>
2024-05-22 15:26:24 +0200

    chore: Update .gitignore and add new files to ignore list

---
+++ .github/workflows/local.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -eo pipefail
+
+usage() {
+    echo "Usage: $0 -e ENV_PATH"
+    exit 1
+}
+
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -e)
+            ENV_PATH="$2"
+            shift 2
+        ;;
+        *)
+            usage
+        ;;
+    esac
+done
+
+# Vérifier si les arguments obligatoires sont définis
+if [ -z "$ENV_PATH" ]; then
+    echo "Error: Missing required arguments."
+    usage
+fi
+# export env variables
+export $(grep -v '^#' $ENV_PATH | xargs)
+# import deployment functions
+source ./deployment.sh
+
+# need az cli
+# source .venv/bin/activate
+az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+DB_HOST=$(./keyvault.sh host $POSTGRES_KEYVAULT)
+DB_USERNAME=$(./keyvault.sh username $POSTGRES_KEYVAULT)
+DB_PASSWORD=$(./keyvault.sh password $POSTGRES_KEYVAULT)
+
+source ./context.sh -g $KUBERNETES_RESOURCE_GROUP -n $KUBERNETES_CLUSTER_NAME
+
+APP_NAME="labelstudio"
+helm_release="${APP_NAME}-release"
+namespace="${APP_NAME}-namespace"
+tls_secret_name="ingress-tls-labelstudio"
+postrges_secret_name="postgres-dev-labelstudio"
+
+# creates the namespace and the tls secret
+source ./secrets.sh \
+    -n $namespace \
+    -tls $tls_secret_name \
+    --tls-crt $TLS_CRT \
+    --tls-key $TLS_KEY \
+    -pst $postrges_secret_name \
+    -pgp $PGPASSWORD
+
+echo "Namespace: $namespace"
+echo "Helm release: $helm_release"
+deploy_labelstudio \
+    $namespace \
+    $helm_release \
+    $DOMAIN_NAME \
+    $tls_secret_name \
+    $DB_HOST \
+    $DB_USERNAME \
+    $DB_PASSWORD \
+    $ARM_TENANT_ID \
+    $postrges_secret_name \
+    $PGPASSWORD \
+    $ENV
